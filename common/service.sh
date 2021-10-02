76a591cffafb6d5cbd1e58453549fd590757b4f3
abdulziz altuwijri <twuijri@gmail.com>
2021-10-02 04:50:05 +0300

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -5,3 +5,25 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
+
+
+FOLDER_WH_STOCK=$HOME/WhatsApp
+FOLDER_WH_LINK=$HOME/Android/media/com.whatsapp/WhatsApp
+
+if [[ ! -d $FOLDER_WH_LINK ]]; then
+
+    mkdir -p $FOLDER_WH_LINK
+    
+fi
+
+if [[ -d $FOLDER_WH_STOCK ]]; then
+
+    ln -sf $FOLDER_WH_STOCK $FOLDER_WH_LINK
+    
+fi
+
+
+
+
+echo
+echo "...done"
